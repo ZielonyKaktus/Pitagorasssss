ffa9e9f1ae797cdd8d6af2b2fdfaeeed0894f3ea
Darcia <lapras098@gmail.com>
2022-12-09 09:17:24 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,34 @@ public class Main {
     public static void main(String[] args) {
         double a;
         double b;
-        Scanner console = new Scanner(System.in);
-        System.out.println("Podaj a:");
-        a = console.nextInt();
-        a = Math.pow(2,a);
-        System.out.println("Podaj b:");
-        b = console.nextInt();
-        b = Math.pow(2,b);
-        System.out.println("Pitagorasss to:" + (Math.sqrt(a + b)));
+        double c;
+        int warunek;
+        int i = 1;
+        while (i < 2) {
+            System.out.println(i);
+            Scanner console = new Scanner(System.in);
+            System.out.println("Wybierz: 1 - przyprostokątne 2 - przeciwprostokątna");
+            warunek = console.nextInt();
+            if (warunek == 1) {
+                System.out.println();
+                System.out.println("Podaj a:");
+                a = console.nextInt();
+                a = Math.pow(2, a);
+                System.out.println("Podaj b:");
+                b = console.nextInt();
+                b = Math.pow(2, b);
+                System.out.println("Pitagorasss to:" + (Math.sqrt(a + b)));
+            }
+            if (warunek == 2) {
+                System.out.println("Podaj a:");
+                a = console.nextInt();
+                a = Math.pow(2, a);
+                System.out.println("Podaj c:");
+                c = console.nextInt();
+                c = Math.pow(2, c);
+                System.out.println("Pitagorasss to:" + (Math.sqrt(c - a)));
+            }
+        }
     }
 }
+
